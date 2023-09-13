05c9ca11be58d002de0dc60997df411864c51b9d
Macmac331 <bautistamacmac331@gmail.com>
2023-09-13 22:31:00 +0800

    changed some text

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -5,7 +5,7 @@ function Home(){
             <div className="wrapper">
                 <section>
                     <div className="home-detail"> 
-                        <h1>Hi, I am Marc Albert</h1>
+                        <h1>Hi, I am Mac</h1>
                         <p>Welcome to my personal website! I am passionate about developing software and solving problems.<br></br>This webpage is just a glimpse of my world. Feel free to explore more about me.</p>
                         <a href="https://www.facebook.com/" target="_blank">Download CV</a>
                     </div>
